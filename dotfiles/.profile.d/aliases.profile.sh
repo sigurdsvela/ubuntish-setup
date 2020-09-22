df4257d3794e203f36c061d2509e569805e914a9
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-22 02:12:51 +0200

    .

--- dotfiles/.profile.d/aliases.profile.sh
+++ dotfiles/.profile.d/aliases.profile.sh
@@ -3,14 +3,14 @@ alias reboot-bios="sudo systemctl reboot --firmware-setup"
 alias wsens="watch -n 1 sensors \"it8620-isa-0a60\""
 alias winxi="watch --color -n 1 inxi -F"
 
-alias keyd-start="sudo service keyboardmap start"
-alias keyd-stop="sudo service keyboardmap stop"
-alias keyd-restart="sudo service keyboardmap restart"
-alias keyd-status="sudo service keyboardmap status"
+alias keyd-start="sudo service xkeysnail start"
+alias keyd-stop="sudo service xkeysnail stop"
+alias keyd-restart="sudo service xkeysnail restart"
+alias keyd-status="sudo service xkeysnail status"
 
 keyd-watch() {
-    sudo service keyboardmap stop
+    sudo service xkeysnail stop
     sleep 1
-    sudo xkeysnail /etc/systemd/system/keyboardmap.service.d/xkeysnail.config.py
-    sudo service keyboardmap start
+    sudo /home/sigsve/.config/xkeysnail/xkeystart.sh
+    sudo service xkeysnail start
 }
